2fb257bf885b72de9e2a4e889c5000a2e8ef1114
roxdi <roxdil426@gmail.com>
2025-02-08 16:58:01 +0530

    first commit

---
+++ src/main/java/com/example/githubaction/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.example.githubaction;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController {
+
+    @GetMapping
+    public String getWelcome (){
+        return "Welcome to learn Github Action";
+    }
+}
